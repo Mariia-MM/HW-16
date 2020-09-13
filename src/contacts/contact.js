f1802a6d3b3f05b9fe36c74ba9703ffbe29feb8e
Mariia <mytsko.mariia@gmail.com>
2020-09-13 23:12:21 +0200

    correcting name input

--- src/contacts/contact.js
+++ src/contacts/contact.js
@@ -3,8 +3,7 @@ import React, { Component } from "react";
 const Contact = (props) => (
   <div className="contact">
     <div className="contact-name">
-      {props.firstName}
-      {props.lastName}
+      {props.firstName} {props.lastName}
     </div>
     <div className="contact-phoneNumber">{props.phone}</div>
     <div className="gender">{props.gender}</div>
